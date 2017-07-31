f93c5b2b66451d8ab7b842ede5504430caeef375
Tim Clulow <tim.clulow@vocus.co.nz>
2017-08-01 01:24:10 +1200

    First Commit!

---
+++ config/postcss.config.js
@@ -0,0 +1,35 @@
+'use strict';
+module.exports = (ctx) => ({
+  parser: ctx.file.extname === '.sss' ? 'sugarss' : false,
+  plugins: {
+    'postcss-partial-import': {},
+    'postcss-cssnext': {
+      features: {
+        customProperties: false
+      }
+    },
+    'postcss-each': {},
+    'postcss-mixins': {},
+    'postcss-math': {},
+    'postcss-custom-media': {},
+    'postcss-media-minmax': {},
+    "postcss-svg": { "utf8": true },
+    "postcss-style-guide": {
+      "project": "BELMU",
+      "dest": "index.html",
+      "showCode": true,
+      "themePath": "css/_engine/engine.styleguide.theme/"
+    },
+    'cssnano': ctx.env === 'production' ? {
+      discardComments: {
+        removeAll: true
+      },
+      filterPlugins: false,
+      discardDuplicates: true,
+      discardOverridden: true,
+      minifySelectors: true,
+      mergeRules: true,
+      core: true
+    } : false
+  }
+})
