8e32cb83701c330c0b2f8bed1bc79ef7447b7901
Jon Makoto <makoto@cssandstuff.com>
2017-10-10 14:17:56 +1300

    altered postcss script

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -6,9 +6,10 @@ if (test('-e', '../../src/css/_layout')) { /* do something with dir */ }else{mkd
 if (test('-e', '../../src/css/_modules')) { /* do something with dir */ }else{mkdir('../../src/css/_modules');};
 if (test('-e', '../../src/css/_base')) { /* do something with dir */ }else{mkdir('../../src/css/_utility');};
 
-cp('-Rf', './src/css/_base', '../../src/css/');
-cp('-Rf', './src/css/_engine', '../../src/css/');
-cp('-Rf', './src/css/_layout', '../../src/css/');
-cp('-Rf', './src/css/_module', '../../src/css/');
-cp('-Rf', './src/css/_utility', '../../src/css/');
-cp('-Rf', './src/css/belmu.css', '../../src/css/');
+cp('-Rf', './src/css/_base', '../../src/css');
+cp('-Rf', './src/css/_engine', '../../src/css');
+cp('-Rf', './src/css/_layout', '../../src/css');
+cp('-Rf', './src/css/_module', '../../src/css');
+cp('-Rf', './src/css/_utility', '../../src/css');
+cp('-Rf', './src/css/belmu.css/*/*', '../../src/css');
+cp('./src/css/belmu.css', '../../src/css/');
