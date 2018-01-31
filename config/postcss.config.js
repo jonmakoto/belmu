69da041798f7fb8c3358b4b8ca07252a08a9b3fa
Jon Makoto <makoto@cssandstuff.com>
2018-01-31 13:57:52 +1300

    added better docs

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -1,6 +1,34 @@
+'use strict';
 module.exports = (ctx) => ({
   parser: ctx.file.extname === '.sss' ? 'sugarss' : false,
   plugins: {
-    'postcss-partial-import': {}
+    'postcss-partial-import': {},
+    'postcss-cssnext': {
+      features: {
+        customProperties: false
+      }
+    },
+    'postcss-each': {},
+    'postcss-mixins': {},
+    'postcss-math': {},
+    'postcss-custom-media': {},
+    'postcss-media-minmax': {},
+    "postcss-style-guide": {
+      "project": "BELMU",
+      "dest": "index.html",
+      "showCode": true,
+      "themePath": "css/_engine/_defaults/engine.styleguide.theme/"
+    },
+    'cssnano': ctx.env === 'production' ? {
+      discardComments: {
+        removeAll: true
+      },
+      filterPlugins: false,
+      discardDuplicates: true,
+      discardOverridden: true,
+      minifySelectors: true,
+      mergeRules: true,
+      core: true
+    } : false
   }
 })
