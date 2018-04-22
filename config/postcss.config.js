b297ab01653dbd285f55e3a05087613cadc2680e
Jon Makoto <makoto@cssandstuff.com>
2018-04-23 09:37:05 +1200

    changed engine to element

--- config/postcss.config.js
+++ config/postcss.config.js
@@ -17,7 +17,7 @@ module.exports = (ctx) => ({
       "project": "BELMU",
       "dest": "index.html",
       "showCode": true,
-      "themePath": "css/belmu/_engine/_defaults/engine.styleguide.theme/"
+      "themePath": "css/belmu/_base/_styleguide.theme/"
     },
     'cssnano': ctx.env === 'production' ? {
       discardComments: {
