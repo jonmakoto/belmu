3edcd031c958444465c2813bab1dee083f8b85d2
Jon Makoto <makoto@cssandstuff.com>
2018-06-18 17:14:41 +1200

    starting to migrate to sapper

---
+++ postcss.config.js
@@ -0,0 +1,21 @@
+'use strict';
+module.exports = (ctx) => ({
+  parser: ctx.file.extname === '.sss' ? 'sugarss' : false,
+  plugins: {
+    'postcss-partial-import': {},
+    'mdcss': {
+      'theme': require('mdcss-theme-sapper'),
+      'destination':'./routes'
+    },
+    'postcss-cssnext': {
+      features: {
+        customProperties: false
+      }
+    },  
+    'postcss-each': {},
+    'postcss-mixins': {},
+    'postcss-math': {},
+    'postcss-custom-media': {},
+    'postcss-media-minmax': {}
+  }
+})
