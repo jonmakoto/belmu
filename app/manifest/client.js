4e698aa55d35035ab0a22e6ebfe4a4d851621189
Jon Makoto <makoto@cssandstuff.com>
2018-06-18 23:55:43 +1200

    dark bg themed

--- app/manifest/client.js
+++ app/manifest/client.js
@@ -5,7 +5,7 @@ export const routes = [
 ];
 
 if (module.hot) {
-	import('/Users/me/Sites/OLD_Sites/belmu/node_modules/sapper/sapper-dev-client.js').then(client => {
-		client.connect(10000);
+	import('/Users/tim.clulow/Documents/_git/belmu/node_modules/sapper/sapper-dev-client.js').then(client => {
+		client.connect(10003);
 	});
 }
